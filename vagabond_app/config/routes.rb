a0763c6e970dfae19c4b27ae0433cee4afa04fa0
Andrew <andrew.vinocur@gmail.com>
2016-10-06 16:52:26 -0700

    adds post_users#index

--- vagabond_app/config/routes.rb
+++ vagabond_app/config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   get "/posts", to: "posts#index", as: "posts"
   get "/posts/:id", to: "posts#show", as: "post"
+
+  get "/users/:user_id/posts", to: "post_users#index", as: "user_posts"
+  
 end
