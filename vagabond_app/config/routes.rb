a8f4959e0dc19f5355b6d7cd93239f5cd143b7a6
Andrew <andrew.vinocur@gmail.com>
2016-10-06 16:34:36 -0700

    adds haml gem and initial post routes for index and show

--- vagabond_app/config/routes.rb
+++ vagabond_app/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "posts#index"
+
+  get "/posts", to: "posts#index", as: "posts"
+  get "/posts/:id" to: "posts#show", as: "post"
 end
