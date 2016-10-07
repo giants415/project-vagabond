199ecf06ccd098302bce2940563ecf399fe7d7ca
Andrew <andrew.vinocur@gmail.com>
2016-10-07 13:56:19 -0700

    FOUNDATION WORKS

--- vagabond_app/app/assets/javascripts/application.js
+++ vagabond_app/app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require foundation-rails
 //= require jquery
 //= require jquery_ujs
 //= require foundation
