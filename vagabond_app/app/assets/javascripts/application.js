c9d558d5521b2739e068e9c8a891dd657f872546
Andrew <andrew.vinocur@gmail.com>
2016-10-07 12:34:29 -0700

    post-foundation install and set up

--- vagabond_app/app/assets/javascripts/application.js
+++ vagabond_app/app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require foundation-rails
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require turbolinks
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
