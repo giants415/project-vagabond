9a72abbad51d04e645419cd93e9478611c74aa4e
Andrew <andrew.vinocur@gmail.com>
2016-10-06 17:30:52 -0700

    adds new post route

--- vagabond_app/config/routes.rb
+++ vagabond_app/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root "posts#index"
 
   get "/posts", to: "posts#index", as: "posts"
-  get "/posts/:id", to: "posts#show", as: "post"
+  get "/posts/new", to: "posts#new", as: "new_post"
   post "/posts", to: "posts#create"
+  get "/posts/:id", to: "posts#show", as: "post"
 
   get "/users/:user_id/posts", to: "post_users#index", as: "user_posts"
   
