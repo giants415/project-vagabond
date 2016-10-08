d5315c41582939cd595e19574ffb1006be455825
wnamen <williamjosephnamen@gmail.com>
2016-10-07 18:19:12 -0700

    Added Fakker and removed post_user relationship from schema

--- vagabond_app/db/migrate/20161006235845_create_post_users.rb
+++
@@ -1,8 +0,0 @@
-class CreatePostUsers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :post_users do |t|
-
-      t.timestamps
-    end
-  end
-end
--- vagabond_app/db/schema.rb
+++ vagabond_app/db/schema.rb
@@ -22,11 +22,6 @@ ActiveRecord::Schema.define(version: 20161007215324) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "post_users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
