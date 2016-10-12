0367f584db80d80fecbdf3fc82b88f34841d2ef2
wnamen <williamjosephnamen@gmail.com>
2016-10-12 12:51:27 -0700

    completed seed file and button bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,9 @@
 
 
 City.destroy_all
+User.destroy_all
+Post.destroy_all
+
 City.create({city_name: "San Francisco", city_state: "California", city_image: "sf-image.jpg"})
 City.create({city_name: "Gibraltar", city_state: "Gibraltar", city_image: "gibraltar-image.jpg"})
 City.create({city_name: "London", city_state: "United Kingdom", city_image: "london-image.jpg"})
@@ -31,5 +34,14 @@ end
     username: FFaker::InternetSE.login_user_name,
     password_digest: FFaker::InternetSE.password,
     city: FFaker::AddressBR::city
+  )
+end
+
+100.times do
+  Post.create(
+    title: FFaker::HipsterIpsum.sentence,
+    content: FFaker::HipsterIpsum.paragraph,
+    city_id: Random.rand(1..3).to_s,
+    user_id: Random.rand(1..(User.count)).to_s
   )
 end
