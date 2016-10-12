c39ec2ecf13ad75eb96dec85fc2eb7975c9967e0
wnamen <williamjosephnamen@gmail.com>
2016-10-12 12:01:50 -0700

    fixed edit bugs and cleaned site

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :logged_in?, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @posts = Post.where(city_id: params[:city_id]).order(created_at: :desc)
